5b8d58475eaf4b073498eb89fc374026d17f902e
Deivid Blanco <dblancoh17_1@unc.edu.pe>
2026-06-22 19:39:17 -0500

    agregando primero archivos al back

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306, // Añade esta línea
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: { rejectUnauthorized: false } // Aiven suele requerir SSL activo
+});
+
+
+module.exports = pool;
